5cb2e170765c57607223db9cc25c5cfec4689607
ddo6 <ddo6@njit.edu>
2017-03-21 18:44:57 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=localhost;dbname=ddo6';
+    $username = 'ddo6';
+    $password = 'VP1tk3lrA';
 
     try {
         $db = new PDO($dsn, $username, $password);
